3c8b5988699917729b0afe99e35c42e954439e53
Brosflask <hakbrosfl@gmail.com>
2025-12-06 17:58:00 +0000

    Update MainApp.java

--- src/main/java/com/uhi_visualizer/MainApp.java
+++ src/main/java/com/uhi_visualizer/MainApp.java
@@ -1,6 +1,6 @@
 /*
  * Name of project: UHI Visualizer
- * Names of authors: Jachin Hugh Dzidumor Kpogli, Curtis Atudedam Asizem, Adama Baba, Olivia Gyanwah Panford
+ * Names of authors: Jachin Hugh Dzidumor Kpogli, Curtis Asizem, Adama Baba, Olivia Gyanwah Panford
  * Date created: November 15, 2025
  * Purpose: To display statistical information for a UHI (Urban Heat Island)
  */
